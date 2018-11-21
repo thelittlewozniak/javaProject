0388f62f545c2fca372aa936a53c5317d9d1fe78
thelittlewozniak <nathan.pire@gmail.com>
2018-11-21 14:43:09 +0100

    finishing loginView

--- Project/NathanPire/src/be/nathanPire/Views/LoginView.java
+++ Project/NathanPire/src/be/nathanPire/Views/LoginView.java
@@ -7,9 +7,26 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import be.nathanPire.BusinessLayer.PlayerBusiness;
+import be.nathanPire.pojo.Player;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class LoginView extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7072452054227634098L;
 	private JPanel contentPane;
+	private JTextField txtEmail;
+	private JTextField txtPassword;
 
 	/**
 	 * Launch the application.
@@ -35,8 +52,58 @@ public class LoginView extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Login");
+		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		lblLogin.setBounds(10, 11, 414, 54);
+		contentPane.add(lblLogin);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblEmail.setBounds(10, 76, 149, 54);
+		contentPane.add(lblEmail);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblPassword.setBounds(10, 141, 149, 54);
+		contentPane.add(lblPassword);
+		
+		txtEmail = new JTextField();
+		txtEmail.setHorizontalAlignment(SwingConstants.LEFT);
+		txtEmail.setBounds(217, 76, 207, 54);
+		contentPane.add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		txtPassword = new JTextField();
+		txtPassword.setHorizontalAlignment(SwingConstants.LEFT);
+		txtPassword.setColumns(10);
+		txtPassword.setBounds(217, 141, 207, 54);
+		contentPane.add(txtPassword);
+		
+		JButton btnConnect = new JButton("Connect");
+		btnConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(txtEmail.getText()!=null && txtPassword.getText()!=null) {
+					PlayerBusiness p=new PlayerBusiness();
+					Player u=p.Login(txtEmail.getText(), txtPassword.getText());
+					if(u!=null) {
+						
+					}
+				}
+			}
+		});
+		btnConnect.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnConnect.setBounds(10, 206, 207, 44);
+		contentPane.add(btnConnect);
+		
+		JButton btnNoAccount = new JButton("No account?");
+		btnNoAccount.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnNoAccount.setBounds(217, 206, 207, 44);
+		contentPane.add(btnNoAccount);
 	}
-
 }
